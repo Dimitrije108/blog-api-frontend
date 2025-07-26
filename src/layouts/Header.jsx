cf175b92d3b4b4e3bfd2d3238357f2c92e78b0ce
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-07-26 11:49:46 +0200

    feat: implement Header component with logout and github buttons

---
+++ src/layouts/Header.jsx
@@ -0,0 +1,47 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthProvider";
+import githubIcon from "/src/assets/icons/github-mark.svg";
+import logoutIcon from "/src/assets/icons/logout.svg";
+
+// TODO:
+// - Render logout button only if user is logged in
+// - Render login button if user is not logged in
+// - If logged in user is admin/author render a CMS page button
+
+export default function Header() {
+	const navigate = useNavigate();
+	const { logout } = useAuth();
+	// Logout user
+	const handleLogout = () => {
+		logout();
+		navigate("/login");
+	};
+
+	return (
+		<header className="p-4 pt-6 flex items-center">
+			<h1 className="mx-auto font-['Midland-Regular']">
+				Archaeoblog
+			</h1>
+			<Link 
+				to="https://github.com/Dimitrije108/blog-api-cms"
+				className="w-6"
+			>
+				<img 
+					src={githubIcon}
+					alt="github icon" 
+					title="GitHub"
+				/>
+			</Link>
+			<button 
+				className="w-6 p-0 ml-4"
+				onClick={handleLogout}
+			>
+				<img 
+					src={logoutIcon}
+					alt="logout icon" 
+					title="Logout"
+				/>
+			</button>
+		</header>
+	)
+};
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,6 +1,6 @@
 import { useNavigation } from "react-router-dom";
 import { Outlet } from "react-router-dom";
-// import Header from "./Header";
+import Header from "./Header";
 import Spinner from "../components/Spinner";
 
 export default function Layout() {
@@ -10,7 +10,7 @@ export default function Layout() {
 
   return (
     <div className="mx-auto lg:max-w-[90rem] h-screen">
-      {/* <Header /> */}
+      <Header />
       <main className="pl-10 pr-10 pb-14">
         {isLoading
           ? <Spinner />
